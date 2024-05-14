cbe467f816b1d859d39439fb11438c67d45eafc8
lypavel <157053921+lypavel@users.noreply.github.com>
2024-05-14 15:13:28 +0300

    Setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -113,6 +113,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'frontend'
 ]
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
